da7a408bf9a8fc9a9c1c9413740e02e054fea9a9
selenyel <selenyelyazilim@gmail.com>
2023-08-03 17:52:09 +0300

    Lesson 216. Reactive: Setting and Patching Values
    setValue,
    patchValue,
    reset,
    forbiddenEmailsWithObservable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,18 +29,34 @@ export class AppComponent {
     //   (value) => console.log(value)
     // );
 
-    
+
     // This is status changes. It controls status of the form everytime form elements changes on key stroke 
 
     // this.signupForm.statusChanges.subscribe(
     //   (value) => console.log(value)
     // );
 
+    this.signupForm.setValue({
+      'userData': {
+        'username' : 'Burak',
+        'email' : 'myUsernameWasBurak@outlook.com'
+      },
+      'gender' : 'male',
+      'hobbies' : []
+    })
+
+    this.signupForm.patchValue({
+      'userData': {
+        'username' : 'Selen'
+      }
+    })
+
   }
   
 
   onSubmit(){
     console.log('my form', this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby(){
@@ -71,4 +87,66 @@ export class AppComponent {
     })
     return promise;
   }
+
+  forbiddenEmailsWithObservable(control: FormControl) {
+    const observable = new Observable(observer => {
+        setTimeout(() => {
+            if(control.value === 'test@test.com') {
+                observer.next({
+                  emailIsForbidden: true
+                })
+            } else {
+                observer.next(null);
+            }
+            observer.complete();
+        }, 1500)
+    });
+    return observable;
+  }
 }
+
+
+
+
+// Assumed you have already worked through section 18 of this course, you can also try the email validation with a real Http request (remember that Angular's Http Observables are completed automatically after the first event emission!).
+
+// In your Firebase database create a node forbidden-email: "test@test.com".
+
+// And add/replace these code lines in the final app of the forms section:
+
+
+// app.module.ts
+
+// import { HttpClientModule } from '@angular/common/http';
+
+
+// app.component.ts
+
+// import { HttpClient } from '@angular/common/http';
+// import { map } from 'rxjs/operators';
+ 
+// ...
+ 
+// constructor(private http: HttpClient) {}
+ 
+// ...
+ 
+// 'email': new FormControl(
+//   null, 
+//   [Validators.required, Validators.email],
+//   this.forbiddenEmails.bind(this)
+// )
+ 
+// ...
+ 
+// forbiddenEmails(control: FormControl): Observable<any> {
+//   return this.http
+//     .get<string>('https://xyz.firebaseio.com/forbidden-email.json')
+//     .pipe(
+//       map(
+//         email => email === control.value 
+//           ? {emailIsForbidden: true} 
+//           : null
+//       )
+//     );
+// }
